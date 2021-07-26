fcf353d872192465a2b6ba7736b7f62ee108f5d6
Nils Magne Lunde <nilsml@nilsland.com>
2021-07-26 22:02:02 +0200

    ✅ Introducing tests

---
+++ src/axe-result-parser.test.ts
@@ -0,0 +1,25 @@
+import test from 'tape'
+import { parse } from './axe-result-parser'
+import json from '../example-files/result.json'
+
+test('Empty json does not crash', (t) => {
+  t.assert(parse(undefined))
+  t.assert(parse(null))
+  t.assert(parse({}))
+  t.end()
+})
+
+test('Invalid json does not crash', (t) => {
+  t.assert(parse({ foo: 'bar' } as unknown as any[]))
+  t.end()
+})
+
+test('Valid json does not crash', (t) => {
+  t.assert(parse(json))
+  t.end()
+})
+
+test('3 violations are found in json', (t) => {
+  t.plan(1)
+  t.equal(parse(json).violations.count, 3)
+})
