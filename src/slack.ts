8993c6a9d4242700f917c6434e3502edccc884d7
Nils Magne Lunde <nilsml@nilsland.com>
2021-07-27 20:13:50 +0200

    🚧 Intermediate commit while working on Slack part

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,30 @@
 import { none, some, match, fromNullable } from 'fp-ts/lib/Option'
-import { pipe } from 'fp-ts/lib/function'
+import { pipe, flow } from 'fp-ts/lib/function'
 import { IncomingWebhook } from '@slack/webhook'
+import { Result } from './types'
 
-const sendMessageToSlack = (url: string): number => 0
+type FromTemplateType = (numberOfViolations: number, numberOfIncomplete: number) => string
+const fromTemplate: FromTemplateType = (numberOfViolations, numberOfIncomplete) =>
+  `Number of violations: ${numberOfViolations}\nNumber of incomplete: ${numberOfIncomplete}`
 
-export const send = (maybeUrl: (string | undefined)) => pipe(
-  fromNullable(maybeUrl),
-  match(
-    () => 1,
-    (url: string) => sendMessageToSlack(url),
-  ),
-)
+type PrepareMessageType = (axeResult: Result) => string
+const prepareMessage: PrepareMessageType = (axeResult) =>
+  fromTemplate(axeResult.numberOfViolations, axeResult.numberOfIncomplete)
+
+// type SendMessageToSlackType = (url: string, axeResult: Result) => number // TODO: Replace with proper success type
+// const sendMessageToSlack: SendMessageToSlackType = (url, axeResult) => flow(
+//   connectToSlack(url)
+//   prepareMessage(axeResult),
+//   sendMessageToSlack
+// )
+
+// type SendType = (maybeUrl: (string | undefined), axeResult: Result) => number
+// export const send: SendType = (maybeUrl, axeResult) =>
+//   pipe(
+//     fromNullable(maybeUrl),
+//     match(
+//       () => 1,
+//       (url: string) => sendMessageToSlack(url, axeResult),
+//     ),
+//   )
 // const webhook = new IncomingWebhook(url)
